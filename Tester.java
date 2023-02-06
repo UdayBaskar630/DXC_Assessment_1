530e92feb4a11ea9e8bc1b54f1c4e3095d37260f
UdayBaskar630 <68768804+UdayBaskar630@users.noreply.github.com>
2023-02-06 12:45:21 +0530

    Add files via upload

--- Tester.java
+++ Tester.java
@@ -1,54 +1,23 @@
-package DXC_Team;
+package Q3;
 
-import java.util.Scanner;
 
-public class Tester 
+public class Tester
 {
-    public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
-        TShirt[] tShirts = 
-        	{
-                new TShirt(200, "Round Neck"),
-                new TShirt(250, "Collared"),
-                new TShirt(300, "Hooded")
-        	};
-        int[] purchase_cart = new int[3];
 
-        for (int i = 0; i < 3; i++)
-        {
-            System.out.print(" Enter the number of " + tShirts[i].getType() + " T-Shirts: ");
-            purchase_cart[i] = sc.nextInt();
-        }
+	
+		public static void main(String[] args)
+		{
+		    Point p1 = new Point();
+		    p1.setX(2);
+		    p1.setY(3);
+		    System.out.println("The distance of P1 from the origin is " + (p1.distance()));
+		    
+		    Point p2 = new Point();
+		    p2.setX(5);
+		    p2.setY(6);
+		    System.out.println("The distance between p1 from p2 is " + (p1.distance(p2)));
+		  }
 
-        int totalCost = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            totalCost = totalCost + (tShirts[i].getPrice() * purchase_cart[i]);
-        }
+	}
 
-        int totalOrdered = 0;
-        for (int i = 0; i < 3; i++)
-        {
-            totalOrdered = totalOrdered + purchase_cart[i];
-        }
 
-        int discount = 0;
-        if (totalOrdered < 5)
-        {
-            discount = 0;
-        }
-        else if (totalOrdered >= 5 && totalOrdered <= 10)
-        {
-            discount = 10;
-        } 
-        else 
-        {
-            discount = 20;
-        }
-
-        int finalAmount = (totalCost - (totalCost * (discount / 100)));
-
-        System.out.println("  The final amount after discount is: Rs." + finalAmount);
-    }
-}
